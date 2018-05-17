d66ddc7faa19af6181d204135ae9c2b4162a6aa2
ibkaufma <markus.kaufmann@stud.hslu.ch>
2018-05-17 18:33:01 +0200

    no message

--- bingewatcher/src/pages/about/about.ts
+++ bingewatcher/src/pages/about/about.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {NavController, Slides} from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  @ViewChild(Slides) slides: Slides;
+
+  private today: Date = new Date(Date.now());
+  private tomorrow: Date = new Date(Date.now());
+  private tomorrowpp: Date = new Date(Date.now());
+
+  dateMap = new Map<number, Date>();
+  currentIndex = 0;
+
+  sampleData = Array(20).fill("sample data");
 
   constructor(public navCtrl: NavController) {
-
+    this.initialize();
   }
 
+  private initialize() {
+    this.tomorrow.setDate(this.today.getDate() + 1);
+    this.tomorrowpp.setDate(this.today.getDate() + 2);
+
+    this.dateMap.set(0, this.today);
+    this.dateMap.set(1, this.tomorrow);
+    this.dateMap.set(2, this.tomorrowpp);
+  }
+
+  getCurrentDate(): string {
+    return this.dateMap.get(this.currentIndex).toLocaleDateString("de-CH");
+  }
+
+  slideChanged() {
+    this.currentIndex = this.slides.getActiveIndex();
+  }
 }
