ae37cc894a5a6bde6d181162a9584d9078100c54
ibkaufma <markus.kaufmann@stud.hslu.ch>
2018-05-28 10:49:16 +0200

    altered home tab

--- bingewatcher/src/interfaces/Episode.ts
+++ bingewatcher/src/interfaces/Episode.ts
@@ -1,8 +1,12 @@
 export interface Episode {
-  series_name: string
-  episode_number: number
-  name: string
-  overview: string
-  season_number: number
-  still_path: string
+  id: number;
+  air_date: string;
+  series_id: number;
+  series_name: string;
+  episode_number: number;
+  name: string;
+  overview: string;
+  season_number: number;
+  still_path: string;
+  production_code: string;
 }
--- bingewatcher/src/pages/home/home.ts
+++ bingewatcher/src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {NavController, Slides} from 'ionic-angular';
 import {HttpClient} from "@angular/common/http";
 import {SeriesAiringToday} from "../../interfaces/SeriesAiringToday";
 import {SerieAiringToday} from "../../interfaces/SerieAiringToday";
@@ -16,13 +16,23 @@ export class HomePage {
   private static readonly API_URL = "https://api.themoviedb.org/3/tv/tv_placeholder/season/season_placeholder/episode/episode_placeholder?api_key=a3098e7a0c2ad40520eab2ff867b1f76&language=en-US";
   private static readonly IMAGE_URL = "http://image.tmdb.org/t/p/w185/";
 
+  @ViewChild(Slides) slides: Slides;
+
   series: Array<SerieAiringToday>;
   seriesToday: Array<Episode>;
-  seriesTomorrow: Array<SerieAiringToday>;
-  seriesDayAfterTomorrow: Array<SerieAiringToday>;
+  seriesTomorrow: Array<Episode>;
+  seriesDayAfterTomorrow: Array<Episode>;
+
+  currentIndex = 1;
+
+  seriesMap = new Map<number, Episode>();
 
   constructor(public navCtrl: NavController, public httpClient: HttpClient) {
     this.seriesToday = [];
+    this.getData();
+  }
+
+  private getData() {
     let seriesJsonToday = this.httpClient.get('http://127.0.0.1:8000/?date=28_05_2018');
     seriesJsonToday.subscribe(data => {
       let seriesAiringToday: SeriesAiringToday = <SeriesAiringToday>data;
@@ -40,23 +50,25 @@ export class HomePage {
           let episode_request = this.httpClient.get(url);
           episode_request.subscribe(value => {
             let episode: Episode = <Episode>value;
-            episode.series_name = serie.name;
-            console.log(episode);
+            episode.series_id = serie_id.id;
+            episode.series_name = serie_id.name;
             if (episode.still_path) {
-              episode.still_path = HomePage.IMAGE_URL + episode.still_path
+              episode.still_path = HomePage.IMAGE_URL + serie_id.poster_path;
             } else  {
-              episode.still_path = "./assets/imgs/no_poster_detail.png"
+              episode.still_path = "./assets/imgs/no_poster.png";
             }
             this.seriesToday.push(episode)
-          })
+          });
         });
       }
     });
   }
 
-  serieSelected(series: Episode) {
+  serieSelected(episode: Episode) {
 
   }
 
-
+  slideChanged() {
+    this.currentIndex = this.slides.getActiveIndex();
+  }
 }
